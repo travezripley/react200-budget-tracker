b89310069954288bcd811e2028a1230c79c95bce
Travis Ripley <theripleys@gmail.com>
2019-05-13 11:30:47 -0700

    index corrected

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-dom";
+import { Provider } from "react-redux";
 import App from "./app";
 import store from "./rootStore";
 
